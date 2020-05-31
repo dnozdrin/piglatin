1ea34d8450ee001974d50c9a1b77fa7386aed605
Dmytro Nozdrin <nozdrin.dmytro@gmail.com>
2020-05-31 18:43:06 +0300

    Log to stdin logger init errors

--- logger/error.go
+++ logger/error.go
@@ -12,17 +12,17 @@ var Error *log.Logger
 func init() {
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
 
 	absPath, err := filepath.Abs(dir + "/log")
 	if err != nil {
-		log.Fatalf("Error reading given path: %s", err)
+		log.Printf("Error reading given path: %s", err)
 	}
 
 	file, err := os.OpenFile(absPath+"/error.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		log.Fatalf("Error opening file: %s", err)
+		log.Printf("Error opening file: %s", err)
 	}
 
 	Error = log.New(file, "\t", log.Ldate|log.Ltime|log.Lshortfile)
